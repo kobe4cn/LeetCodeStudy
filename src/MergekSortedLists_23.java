fcc3aef6ad756f9e0a28e23ba65e03cedfda9757
kobe4cn <kevin.yang.xgz@gmail.com>
2022-05-06 23:19:28 +0800

    通过PriorityQueue 实现的两个队列算法 中位数其实就是单数和双数之分，单数为中间的一个数字，双数就是中间位置的左右数相加
    初始化两个优先队列，每次的数据都先增加进行以顺序排列的PriorityQueue，每次增加进max队列的数据都会经过排序，通过remove将最小的数据存入min，但是有一个判断，为了确保min和max相差1位，当min比max队列数据多的时候将min 最大的move回max，这样max的第一位一定确保是中位数当数据总数为单数的时候。如果min和max为双数的话，那就是min的为从大到小排序的第一位，以及max排序为从小到大的第一位只和除以2即为中位数

---
+++ src/MergekSortedLists_23.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+public class MergekSortedLists_23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+//        PriorityQueue<ListNode> newqueue=new PriorityQueue<>(new Comparator<ListNode>() {
+//            @Override
+//            public int compare(ListNode o1, ListNode o2) {
+//                return o2.val-o1.val;
+//            }
+//        });
+        List<Integer> listnodelist = new ArrayList<>();
+        for (int i = 0; i < lists.length; i++) {
+            ListNode node = lists[i];
+            if (node == null) {
+                continue;
+            }
+            listnodelist.addAll(getListNode(node));
+        }
+
+        return serListNode(listnodelist);
+    }
+
+    public static List<Integer> getListNode(ListNode node) {
+        List<Integer> listnodelist = new ArrayList<>();
+        if (node != null) {
+            //System.out.println(node.val);
+            listnodelist.add(node.val);
+            ListNode nodenext = node.next;
+            while (nodenext != null) {
+//                ListNode temp = nodenext;
+//                System.out.print("list:   " +getListNode(temp).toString());
+//                listnodelist.addAll(getListNode(temp));
+                listnodelist.add(nodenext.val);
+                nodenext = nodenext.next;
+            }
+        }
+        System.out.print(listnodelist.toString());
+        return listnodelist;
+    }
+
+    public static ListNode serListNode(List<Integer> listnodelist) {
+        List<Integer> integerList = listnodelist.stream().sorted().collect(Collectors.toList());
+        System.out.println(integerList.toString());
+        ListNode listNode1 = new ListNode(0);
+        ListNode listNode = listNode1;
+        int i = 0;
+        while (i < integerList.size()) {
+            ListNode temp = new ListNode(integerList.get(i));
+            listNode.next = temp;
+            listNode = listNode.next;
+            i++;
+        }
+        return listNode1.next;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3, 4, 4, 5, 6};
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        ListNode node = serListNode(list);
+        System.out.println(node);
+    }
+
+}
