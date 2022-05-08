cf2d7b6978d3465b710b3f26777c62dc245cc779
kobe4cn <kevin.yang.xgz@gmail.com>
2022-05-08 23:24:09 +0800

    commit

---
+++ src/CopyListwithRandomPointer_138.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CopyListwithRandomPointer_138 {
+    public Node copyRandomList(Node head) {
+        if(head==null){
+            return head;
+        }
+        Node node=head;
+        Map<Node,Node> map=new HashMap<>();
+        while(node!=null){
+            Node clone=new Node(node.val);
+            map.put(node,clone);
+            node=node.next;
+        }
+        node=head;
+        while(node!=null){
+            map.get(node).next=map.get(node.next);
+            map.get(node).random=map.get(node.random);
+            node=node.next;
+        }
+        return map.get(head);
+
+    }
+}
