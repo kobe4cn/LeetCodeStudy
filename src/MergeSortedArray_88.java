97ecc63dcd989b73fcffc0716b67fe14b0bcf1cb
kobe4cn <kevin.yang.xgz@gmail.com>
2022-05-24 15:04:33 +0800

    commit

---
+++ src/MergeSortedArray_88.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//88. 合并两个有序数组
+public class MergeSortedArray_88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k=0;
+        for (int i:nums2
+             ) {
+            nums1[m+k]=i;
+            k++;
+        }
+        Arrays.sort(nums1);
+    }
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayToString(int[] nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            int number = nums[index];
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums1 = stringToIntegerArray(line);
+            line = in.readLine();
+            int m = Integer.parseInt(line);
+            line = in.readLine();
+            int[] nums2 = stringToIntegerArray(line);
+            line = in.readLine();
+            int n = Integer.parseInt(line);
+
+            new MergeSortedArray_88().merge(nums1, m, nums2, n);
+            String out = integerArrayToString(nums1);
+
+            System.out.print(out);
+        }
+    }
+}
