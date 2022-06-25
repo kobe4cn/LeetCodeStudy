3c16564bb66315918c033a99b86ffd8aceeae62f
kobe4cn <kevin.yang.xgz@gmail.com>
2022-06-26 02:01:49 +0800

    commit

--- src/Random_Pick_with_Blacklist_710.java
+++ src/Random_Pick_with_Blacklist_710.java
@@ -24,6 +24,46 @@ public class Random_Pick_with_Blacklist_710 {
         return poll.getKey();
     }
 
+    class Solution {
+
+        private final int n;
+
+        private final Random random = new Random();
+
+        private final Map<Integer, Integer> map;
+
+        public Solution(int n, int[] blacklist) {
+            // 尝试通过二分来找出黑名单的数, 二分失败
+            // 用评论区看到的方法, 假设黑名单长度为m
+            // 假设[0:n-m)有x个黑名单, 那么[n-m:n)有m-x个黑名单, 则说明[n-m:n)中有m-(m-x)=x个白名单
+            // 因此可以将x个黑名单映射到[n-m:n)的白名单中, 然后只需要找[0:n-m)中的数即可, 再判断当前数
+            // 是否映射到了[n-m:n)的白名单中
+            this.n = n - blacklist.length;
+            this.map = new HashMap<>(blacklist.length);
+            // 先将黑名单数加入到set中
+            Set<Integer> blacklistSet = new HashSet<>();
+            for (int black : blacklist) {
+                blacklistSet.add(black);
+            }
+            int rightRegionIndex = this.n;
+            for (int black : blacklist) {
+                if (black < this.n) {
+                    while (blacklistSet.contains(rightRegionIndex)) {
+                        rightRegionIndex++;
+                    }
+                    this.map.put(black, rightRegionIndex++);
+                }
+            }
+        }
+
+        public int pick() {
+            int randomValue = this.random.nextInt(this.n);
+            Integer value = this.map.get(randomValue);
+            return value == null ? randomValue : value;
+        }
+
+    }
+
 
     public static void main(String[] args) {
         int[] blacklist = {2,1};
