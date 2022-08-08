c8dc7c7b7550f5a68426d3726c1f89eaac86db6c
kobe4cn <kevin.yang.xgz@gmail.com>
2022-08-09 00:23:15 +0800

    commit

--- src/Exclusive_Time_of_Functions_636.java
+++ src/Exclusive_Time_of_Functions_636.java
@@ -1,7 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Exclusive_Time_of_Functions_636 {
     public int[] exclusiveTime(int n, List<String> logs) {
+        Deque<int[]> stack = new ArrayDeque<int[]>(); // {idx, 开始运行的时间}
+        int[] res = new int[n];
+        for (String log : logs) {
+            int idx = Integer.parseInt(log.substring(0, log.indexOf(':')));
+            String type = log.substring(log.indexOf(':') + 1, log.lastIndexOf(':'));
+            int timestamp = Integer.parseInt(log.substring(log.lastIndexOf(':') + 1));
+            if ("start".equals(type)) {
+                if (!stack.isEmpty()) {
+                    res[stack.peek()[0]] += timestamp - stack.peek()[1];
+                    stack.peek()[1] = timestamp;
+                }
+                stack.push(new int[]{idx, timestamp});
+            } else {
+                int[] t = stack.pop();
+                res[t[0]] += timestamp - t[1] + 1;
+                if (!stack.isEmpty()) {
+                    stack.peek()[1] = timestamp + 1;
+                }
+            }
+        }
+        return res;
+    }
 
+
+    public static ArrayList<String> stringToStringList(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        String[] parts = input.split(",");
+        ArrayList<String> output = new ArrayList<String>();
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim().replaceAll("\"","");
+            output.add(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayToString(int[] nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            int number = nums[index];
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            line = in.readLine();
+            List<String> logs = stringToStringList(line);
+
+            int[] ret = new Exclusive_Time_of_Functions_636().exclusiveTime(n, logs);
+
+            String out = integerArrayToString(ret);
+
+            System.out.print(out);
+        }
     }
 }
