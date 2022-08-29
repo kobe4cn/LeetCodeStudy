0ad3c41dfbe736277a5f0cb2ac4802edf86391c5
kobe4cn <kevin.yang.xgz@gmail.com>
2022-08-29 13:37:08 +0800

    commit

---
+++ src/Maximum_Width_of_Binary_Tree_662.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//662. 二叉树最大宽度
+public class Maximum_Width_of_Binary_Tree_662 {
+    Map<Integer, Integer> levelMin = new HashMap<Integer, Integer>();
+
+    public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 1, 1);
+    }
+
+    public int dfs(TreeNode node, int depth, int index) {
+        if (node == null) {
+            return 0;
+        }
+        levelMin.putIfAbsent(depth, index); // 每一层最先访问到的节点会是最左边的节点，即每一层编号的最小值
+        return Math.max(index - levelMin.get(depth) + 1, Math.max(dfs(node.left, depth + 1, index * 2), dfs(node.right, depth + 1, index * 2 + 1)));
+    }
+
+
+
+
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            int ret = new Maximum_Width_of_Binary_Tree_662().widthOfBinaryTree(root);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
