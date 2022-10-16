a4a30682ef7c083b87772a53b896af6e4bcc5cf8
kobe4cn <kevin.yang.xgz@gmail.com>
2022-10-16 22:35:47 +0800

    commit

---
+++ src/Possible_Bipartition_886.java
@@ -0,0 +1,92 @@
+import com.eclipsesource.json.JsonArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+//886. 可能的二分法
+public class Possible_Bipartition_886 {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        int[] color = new int[n + 1];
+        List<Integer>[] g = new List[n + 1];
+        for (int i = 0; i <= n; ++i) {
+            g[i] = new ArrayList<Integer>();
+        }
+        for (int[] p : dislikes) {
+            g[p[0]].add(p[1]);
+            g[p[1]].add(p[0]);
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (color[i] == 0 && !dfs(i, 1, color, g)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean dfs(int curnode, int nowcolor, int[] color, List<Integer>[] g) {
+        color[curnode] = nowcolor;
+        for (int nextnode : g[curnode]) {
+            if (color[nextnode] != 0 && color[nextnode] == color[curnode]) {
+                return false;
+            }
+            if (color[nextnode] == 0 && !dfs(nextnode, 3 ^ nowcolor, color, g)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static int[][] stringToInt2dArray(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(input);
+        if (jsonArray.size() == 0) {
+            return new int[0][0];
+        }
+
+        int[][] arr = new int[jsonArray.size()][];
+        for (int i = 0; i < arr.length; i++) {
+            JsonArray cols = jsonArray.get(i).asArray();
+            arr[i] = stringToIntegerArray(cols.toString());
+        }
+        return arr;
+    }
+
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            line = in.readLine();
+            int[][] dislikes = stringToInt2dArray(line);
+
+            boolean ret = new Possible_Bipartition_886().possibleBipartition(n, dislikes);
+
+            String out = booleanToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
